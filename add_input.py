b9f2805489036bb0a354a0a423d7e02cca87642b
bobrov.rom <bobrov.rom@yandex.ru>
2024-12-29 15:11:38 +0300

    add add_input and date_changer

---
+++ add_input.py
@@ -0,0 +1,7 @@
+inputUsername = input("Имя пользователя")
+inputTitle = input("Заголовок")
+inputContent = input("Описание")
+inputStatus = input("Статус") #переделать на буль
+inputCreated_date =  input("Дата начала")
+inputIssue_date =  input("Дата окончания")
+print(inputTitle,inputStatus,inputUsername,inputCreated_date,inputIssue_date)
---
+++ date_changer.py
@@ -0,0 +1,4 @@
+from greetings import created_date, issue_date
+
+print (created_date [0:5])
+print (issue_date [0:5])
