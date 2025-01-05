567c2c7cf67066ce11b0abc7d29fb6312603f41c
bobrov.rom <bobrov.rom@yandex.ru>
2025-01-05 15:21:57 +0300

    fox add_list

---
+++ add_list.py
@@ -0,0 +1,11 @@
+note1 = []
+title = input("Укажите заголовок")
+subTitle = input("Укажите подзаголовок")
+discriptionTitle = input("Укажите описание")
+
+note1.append(title)
+note1.append(subTitle)
+note1.append(discriptionTitle)
+print (note1)
+
+
