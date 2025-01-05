20e1e9a30d8a8c9b4d8a3ded9cc5fbd7f672b919
bobrov.rom <bobrov.rom@yandex.ru>
2025-01-05 15:21:28 +0300

    add final

---
+++ final.py
@@ -0,0 +1,12 @@
+userName = input ("Введите Имя пользователя: ")
+content = input ("Введите описание заметки: ")
+status = input ("Введите статус заметки ('создано','в работе', 'готово' ): ")
+createdDate = input ("Введите дату начала в формате \"день-месяц-год\": ")
+issueDate = input ("Введите дату окончанияв формате \"день-месяц-год\": ")
+title = input ("Введите заголовок заметки: ")
+subTitile = input ("Введите подзоголовок заметки: ")
+note ={
+    'userName':userName,'content':content,'status':status,'createdDate':createdDate[:5],'issueDate':issueDate[:5], "title":
+    [title, subTitile]
+}
+print(note)
